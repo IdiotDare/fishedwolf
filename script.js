002588b746b84fde547ee34fd1da4c09fd3e099e
IdiotDare <89470696+IdiotDare@users.noreply.github.com>
2022-11-19 19:40:24 -0500

    g

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 alert("Not made for mobile. Or really, any screen size besides my own.")
+
 function clickMe(){
     let altText = ["yeetos"] //remember, arrayname.push adds to the end of array
     let paraText = document.getElementById("main")
