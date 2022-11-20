31bb1a5c0db51074f6233aea415c2406c45f9697
IdiotDare <89470696+IdiotDare@users.noreply.github.com>
2022-11-19 19:41:35 -0500

    stupis

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 alert("Not made for mobile. Or really, any screen size besides my own.")
 
 function clickMe(){
-    let altText = ["yeetos"] //remember, arrayname.push adds to the end of array
-    let paraText = document.getElementById("main")
-    if{
-         paraText != altText
-    } then {
+    let altText = ["yeetos"] ;//remember, arrayname.push adds to the end of array
+    let paraText = document.getElementById("main");
+    if(paraText != altText) {
         document.getElementById("main") = altText[0];
         return document.getElementById("main")
     }
